b170ea2e5bbea3a88eebc6521244502c3f684774
javierpoo <javier.poo@gmail.com>
2020-08-15 19:00:07 -0400

    Update game-scene.js

--- src/scenes/game-scene.js
+++ src/scenes/game-scene.js
@@ -207,7 +207,7 @@ class GameScene extends Phaser.Scene {
                 this.sound.play('power');
             }
             else if(score >= 110) {      
-                player.body.setVelocityY(-500); 
+                player.body.setVelocityY(-450); 
                 this.sound.play('jumping');
             }            
             
