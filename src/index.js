43dd756941609a0bf1784da00f67ccc9c0aca3a1
javierpoo <javier.poo@gmail.com>
2020-08-18 18:35:26 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import Phaser from 'phaser';
 import GameScene from './scenes/game-scene';
 
-const height = window.innerHeight;
+//const height = window.innerHeight;
+const height = 600;
 
 const config = {
   type: Phaser.AUTO,
   pixelArt: false,
   roundPixels: false,
-  width: window.innerWidth,
-  height: window.innerHeight,
+  //width: window.innerWidth,
+  //height: window.innerHeight,
+  width: 800,
+  height: 600, 
   parent: 'canvas-container',
   physics: {
       default: 'arcade',
